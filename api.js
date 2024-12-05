57df2c48f6f2a4792e5ffb6671e620d4ff9ba5e6
Cybrix <cybrixtheproot@proton.me>
2024-12-04 20:50:04 -0500

    2024.12.04.0849 sync

--- api.js
+++ api.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 const api = express()
 
+var bodyParser = require('body-parser')
 
 
-
+api.use(bodyParser.urlencoded({ extended: false }))
+api.use(bodyParser.json())
 // Root routes
 
 
@@ -20,9 +22,31 @@ api.get('/platform', (req, res) =>{
     })
 })
 
+api.get('/versions', (req, res) =>{
+    res.send({
+        release: "2024.12.01.1028",
+        preRelease: null,
+        cloudAllowed: ['2024.12.01.1028'],
+        cloudBlacklisted: [],
+        api: '2024.12.04.0436'
+    })
+})
 
-
-
+api.get('/population', (req, res) =>{
+    res.send({
+        platforms: {
+            windows: 0,
+            linux: 0,
+            android: 0,
+            other: 0
+        },
+        makeup:{
+            vr: 0,
+            desktop: 0
+        },
+        total: 0
+    })
+})
 
 
 
@@ -36,6 +60,7 @@ api.all('*', (req, res) =>{
     res.status(404)
     res.send({error: 404})
     console.log('incoming request')
+    console.log(req.headers['user-agent'])
     
 })
 
