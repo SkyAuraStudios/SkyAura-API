a7366692c0ebf2fa67bbfc99d978dbf1b7b9d111
Cybrix <cybrixtheproot@proton.me>
2025-06-09 19:52:04 -0400

    Update api.js

--- api.js
+++ api.js
@@ -3,13 +3,17 @@ const express =  require("express");
 const api = express();
 
 
-api.get('/api/cloudvars/NCR_CONVERSION', (req, res)=>{
+api.get('/api/globalvars/NCR_CONVERSION', (req, res)=>{
     res.send({"ownerId":"GLOBAL","path":"NCR_CONVERSION","value":"0.03448631","partitionKey":"GLOBAL","rowKey":"NCR_CONVERSION","timestamp":"2025-06-09T05:09:48.2479683+00:00","eTag":"W/\"datetime'2025-06-09T05%3A09%3A48.2479683Z'\""})
+})
+
+api.get('/api/globalvars/CDFT_CONVERSION', (req, res)=>{
+    res.send({"ownerId":"GLOBAL","path":"CDFT_CONVERSION","value":"19.66930202","partitionKey":"GLOBAL","rowKey":"CDFT_CONVERSION","timestamp":"2021-11-29T10:47:34.2147849+00:00","eTag":"W/\"datetime'2021-11-29T10%3A47%3A34.2147849Z'\""})
 })
 
 
 
-api.all('/', (req, res)=>{
+api.all('*', (req, res)=>{
     console.log(req.method + " REQUEST AT: " + req.url)
 })
 
