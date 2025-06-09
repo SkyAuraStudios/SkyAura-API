63bc2bbd63675cc0d42bda7f592e9e8c2bbcba27
Cybrix <cybrixtheproot@proton.me>
2025-06-09 19:38:47 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,76 +1,12 @@
-const express = require('express')
+const express =  require("express");
 
-const api = express()
-
-var bodyParser = require('body-parser')
+const api = express();
 
 
-api.use(bodyParser.urlencoded({ extended: false }))
-api.use(bodyParser.json())
-// Root routes
-
-
-api.get('/platform', (req, res) =>{
-    res.send({
-        appName: "SkyAura",
-        supportEmail: "hello@skyaura.net",
-        moderationEmail: "moderation@skyaura.net",
-        supportURL: "https://support.skyaura.net",
-        moderationURL: "https://moderation.skyaura.net",
-
-
-    })
+api.all('/', (req, res)=>{
+    console.log("REQUEST")
 })
 
-api.get('/versions', (req, res) =>{
-    res.send({
-        release: "2024.12.01.1028",
-        preRelease: null,
-        cloudAllowed: ['2024.12.01.1028'],
-        cloudBlacklisted: [],
-        api: '2024.12.04.0436'
-    })
+api.listen(60621, ()=>{
+console.log("LOCAL API INSTANCE Running ON PORT 60621");
 })
-
-api.get('/population', (req, res) =>{
-    res.send({
-        platforms: {
-            windows: 0,
-            linux: 0,
-            android: 0,
-            other: 0
-        },
-        makeup:{
-            vr: 0,
-            desktop: 0
-        },
-        total: 0
-    })
-})
-
-
-
-
-
-
-
-
-
-api.all('*', (req, res) =>{
-    res.status(404)
-    res.send({error: 404})
-    console.log('incoming request')
-    console.log(req.headers['user-agent'])
-    
-})
-
-
-
-
-
-
-
-
-
-
-api.listen(8080)
